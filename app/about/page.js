f39cb5fc293550a653728a1e91b248827bbe6a1e
Shubham <korgaonkarshubham9@gmail.com>
2024-11-13 21:46:30 +0530

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex w-full mb-20 flex-wrap">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-white lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
+            <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+          </div>
+          <div className="flex flex-wrap md:-m-2 -m-1">
+            <div className="flex flex-wrap w-1/2">
+              <div className="md:p-2 p-1 w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
+              </div>
+              <div className="md:p-2 p-1 w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/501x301" />
+              </div>
+              <div className="md:p-2 p-1 w-full">
+                <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/600x360" />
+              </div>
+            </div>
+            <div className="flex flex-wrap w-1/2">
+              <div className="md:p-2 p-1 w-full">
+                <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361" />
+              </div>
+              <div className="md:p-2 p-1 w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302" />
+              </div>
+              <div className="md:p-2 p-1 w-1/2">
+                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-400 bg-gray-900 body-font">
+  <div className="container px-5 py-24 mx-auto flex flex-wrap">
+    <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+      <div className="w-full sm:p-4 px-4 mb-6">
+        <h1 className="title-font font-medium text-xl mb-2 text-white">Moon hashtag pop-up try-hard offal truffaut</h1>
+        <div className="leading-relaxed">Pour-over craft beer pug drinking vinegar live-edge gastropub, keytar neutra sustainable fingerstache kickstarter.</div>
+      </div>
+      <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+        <h2 className="title-font font-medium text-3xl text-white">2.7K</h2>
+        <p className="leading-relaxed">Users</p>
+      </div>
+      <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+        <h2 className="title-font font-medium text-3xl text-white">1.8K</h2>
+        <p className="leading-relaxed">Subscribes</p>
+      </div>
+      <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+        <h2 className="title-font font-medium text-3xl text-white">35</h2>
+        <p className="leading-relaxed">Downloads</p>
+      </div>
+      <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+        <h2 className="title-font font-medium text-3xl text-white">4</h2>
+        <p className="leading-relaxed">Products</p>
+      </div>
+    </div>
+    <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
+      <img className="object-cover object-center w-full h-full" src="https://dummyimage.com/600x300" alt="stats"/>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default About
